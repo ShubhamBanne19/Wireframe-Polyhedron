055001b2abbad334b5faea368aabd58e51cd61e4
ShubhamBanne19 <shubhambanne1819@gmail.com>
2025-12-25 22:49:43 +0530

    comparision operator and data type summary

---
+++ basicsJs/dataType-summary.js
@@ -0,0 +1,32 @@
+//Primitive 
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100; //Number
+const scoreValue = 100.5; //Number
+const isLoggedIn = false; //Boolean
+const outsideTemp = null; //Null
+let userEmail; //Undefined
+
+const id = Symbol("12345"); //Symbol-for unique identifiers
+const anotherId = Symbol("12345");
+
+console.log(id === anotherId); // false
+
+const bigNumber = 12345678901234234567890n; //BigInt
+
+//Reference
+// Object, Array, Function
+
+const heros = ["shaktiman", "naagraj", "doga"]; //Array
+
+let myObj = { //Object
+    name: "Shubham",
+    age: 25,
+}
+
+const myfuntion = function() { //Function
+    console.log("Hello World");
+
+}
+
+console.log(typeof BigInt); // function
